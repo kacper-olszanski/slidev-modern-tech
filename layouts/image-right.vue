e78e83bbbffc153385910f95de0a1d5e03750f20
Kacper Olszanski <kacper.olszanski98@gmail.com>
2023-04-24 12:00:08 +0200

    changes

---
+++ layouts/image-right.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { handleBackground } from '../layoutHelper'
+const props = defineProps({
+  image: {
+    type: String,
+  },
+  class: {
+    type: String,
+  },
+})
+const style = computed(() => handleBackground(props.image))
+</script>
+
+<template>
+  <div class="grid grid-cols-5 w-full h-full">
+    <div class="slidev-layout default image-right" :class="props.class">
+        <div class="my-auto card">
+            <slot />
+        </div>
+
+    </div>
+    <div class="image-right-img" :style="style"></div>
+
+  </div>
+</template>
