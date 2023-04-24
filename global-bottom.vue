541c5df5f0481204f27f8fb772b27417cb1fa526
Kacper Olszanski <kacper.olszanski@dynatrace.com>
2023-04-24 18:22:36 +0200

    minor fixes

--- global-bottom.vue
+++ global-bottom.vue
@@ -1,4 +1,22 @@
 <style>
+.signet {
+  height: 2em;
+  width: 2em;
+}
+.right {
+  display: flex;
+  flex-direction: row;
+  min-width: 6em;
+  justify-content: space-between;
+  align-items: center;
+  align-content: center;
+}
+.spacer {
+  width: 0.125em;
+  height: 80%;
+  border-radius: 25%;
+  background-color: var(--bright-color);
+}
 .foot {
     display: flex;
     justify-content: space-between;
@@ -7,8 +25,11 @@
 <template>
     <footer class="absolute bottom-0 right-0 left-0 p-3 foot">
       <p>{{ $slidev.configs.bottomText }}</p>
-      <div> 
-        <div v-if="logoFooter" :style="logoSrc">
+      <div class="right"> 
+        <div class="signet" v-if="props.logoFooter || $slidev.configs.logoFooter" :style="style">
+          
+        </div>
+        <div class="spacer">
 
         </div>
         <div>
@@ -20,7 +41,7 @@
   
   <script setup lang="ts">
   import { computed, defineProps } from 'vue'
-    import { handleBackground } from './layoutHelper';
+    import { handleBackground, resolveAssetUrl } from './layoutHelper';
   const props = defineProps({
     bottomText: {
         type: String,
@@ -32,5 +53,6 @@
       },
     })
 
-    const logoSrc =  computed(() => handleBackground(props.logoFooter ?? $slidev.configs.logoFooter));
+    const style =  computed(() => handleBackground(props.logoFooter ?? $slidev.configs.logoFooter));
+    const logoSrc =  computed(() => resolveAssetUrl(props.logoFooter ?? $slidev.configs.logoFooter));
   </script>
